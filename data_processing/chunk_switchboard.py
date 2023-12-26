46155e4674ee311ca93260ba27402bac4f44346f
Antón <antondlf@mac.com>
2023-12-26 10:50:33 +0100

    Added functions to remove problematic data

--- data_processing/chunk_switchboard.py
+++ data_processing/chunk_switchboard.py
@@ -1,10 +1,12 @@
 import librosa
 import os
+import shutil
 from pathlib import Path
 import soundfile as sf
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+pd.options.mode.chained_assignment = None
 
 
 feat_labels = {
@@ -66,6 +68,34 @@ def get_turns(audio_id, turn_dir=Path('corpora/nxt_switchboard_ann/csv/turns')):
     return pd.read_csv(Path(turn_dir,audio_id).with_suffix('.csv'))
 
 
+def all_backchannel(root_directory, file_path):
+    """Removes backchannel turns and audios shorter than
+    40 miliseconds from the data.""" 
+    audio_duration = librosa.get_duration(path=file_path)
+    phrase_dir = root_directory / 'phrase' / file_path.with_suffix('.csv').name
+    try:
+        phrase_check = pd.read_csv(phrase_dir)
+    except FileNotFoundError:
+        if audio_duration >= 0.04:
+            return False
+        else:
+            return True
+    
+    if len(phrase_check) > 0:
+        try:
+            return (phrase_check.label == 'backchannel').all(axis=0)
+
+        except AttributeError:
+            print('wrong attribute')
+            return (phrase_check.type == 'backchannel').all(axis=0)
+        
+    elif audio_duration >= 0.04:
+        return False
+    
+    else:
+        return True
+
+
 def adjust_annotations(start, end, audio_id, turn_id, annotation_dir, save_dir,feat_mapping=feat_labels):
     
     
@@ -83,7 +113,7 @@ def adjust_annotations(start, end, audio_id, turn_id, annotation_dir, save_dir,f
         
         else:
             # the case of phones
-            chunk['label'] = [1]* len(chunk)
+            chunk['label'] = [1] * len(chunk)
     
         
         chunk.to_csv(Path(save_dir, feat, turn_id).with_suffix('.csv'))
@@ -114,13 +144,75 @@ def split_wav(audio_id, audio_dir, save_dir, feat_mapping=feat_labels):
         adjust_annotations(start_offset, end_offset, audio_id, turn_id, Path('corpora/nxt_switchboard_ann/csv/'), save_dir, feat_mapping=feat_mapping)
 
 
-def chunk_switchboard_files(dir, feat_mapping=feat_labels):
+def chunk_switchboard_files(root_directory, save_directory, feat_mapping=feat_labels):
 
-    for file in tqdm(list(dir.glob('*.wav'))):
+    split_save = Path('corpora/nxt_switchboard_ann/split_data')
+    for file in tqdm(list(root_directory.glob('*.wav'))):
         audio_id = file.name.split('.')[0]
-        split_wav(audio_id, file, Path('data/switchboard'), feat_mapping=feat_mapping)
+        split_wav(audio_id, file, split_save, feat_mapping=feat_mapping) 
+        
+    print('Removing backchannel phrases')
+    split_wav_files = split_save/'wav'
+    for file in tqdm(list(split_wav_files.glob('*.wav'))):
+
+        if not all_backchannel(save_directory, file):
+            
+            shutil.copy(file, save_directory/ 'wav' / file.name)
 
 
+def cleanup_extra_files(root_directory, wav_directory, new_root):
+    master_list = [file.stem for file in wav_directory.glob('*.wav')]
+    print('Moving CSVs in Master list to data folder')
+    for folder in tqdm(os.listdir(root_directory)):
+        os.makedirs(new_root / folder, exist_ok=True)
+        feat_folder = root_directory / folder
+        for file in feat_folder.glob('*.csv'):
+            
+            if file.stem not in master_list:
+                os.remove(file)
+            else:
+                print(file, new_root / file.name)
+                file.rename(new_root / folder / file.name)
+
+
+def remove_incomplete_annotations(new_root):
+    
+    # TODO: There are smarter and faster ways to do this
+    # remove files where last turn start times are greater than the
+    # last start of any annotation file
+    # Do this before transfering files to data dir.
+    file_list = os.listdir(new_root)
+    empty_list = list()
+    for feat in file_list:
+        feat_files = new_root / feat
+        for file in feat_files.glob('*.csv'):
+            
+            if len(pd.read_csv(file)) == 0:
+                empty_list.append(file.stem)
+    
+    incomplete_files = set(empty_list)
+    
+    for file_id in incomplete_files:
+        
+        for feat in file_list:
+            
+            rm_file = new_root / feat / file_id
+            endpoint = '.wav' if feat == 'wav' else '.csv'
+            os.remove(rm_file.with_suffix(endpoint))
+
+
+def split_move_switchboard():
+    
+    old_root = Path('corpora/nxt_switchboard_ann')
+    new_root = Path('data/switchboard')
+    wav_dir = new_root / 'wav'
+    os.makedirs(wav_dir, exist_ok=True) 
+    chunk_switchboard_files(old_root / 'wav', new_root)
+    split_root = old_root / 'split_data'
+    cleanup_extra_files(split_root, wav_dir, new_root)
+    
+    remove_incomplete_annotations(new_root)
+    
+    
 if __name__ == '__main__':
-
-    chunk_switchboard_files(Path('corpora/nxt_switchboard_ann/wav'))
+    split_move_switchboard()
