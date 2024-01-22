d77bbd9496f79527c20350852f3ff37e208e4345
Antón <antondlf@mac.com>
2024-01-21 16:54:38 -0800

    Migrated all probes to sklearn and skorch

--- run_probes.py
+++ run_probes.py
@@ -8,59 +8,173 @@ from pathlib import Path
 from torch.utils.data import DataLoader
 import lightning as L
 import torch
-from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.utilities.model_summary import ModelSummary
 from lightning.pytorch import loggers as pl_loggers
 from skorch import NeuralNetRegressor, NeuralNetClassifier
+from skorch.callbacks import EarlyStopping, GradientNormClipping
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, log_loss, roc_auc_score, make_scorer, mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score, f1_score, log_loss,\
+roc_auc_score, make_scorer, mean_squared_error, r2_score, precision_score, recall_score
 import numpy as np
+from tqdm import tqdm
 
 
-def log_cross_validation(scores):
+def log_cross_validation(scores, log_dir):
     """Assumed to be a dict output of gc.cv_results_
     or cross_validate().scores"""
-    
+    pd.DataFrame(scores).to_csv(log_dir)
     pass
 
 
-def train_regression(train_data, root_dir):
+def get_full_dataset(data, root_dir):
+    
+    print('Retrieving dataset...')
     feats = list()
     labels = list()
-    for name, group in train_data.groupby('file_id'):
+    for name, group in tqdm(list(data.groupby('file_id'))):
         feat_file = name + '.npy'
         raw_feats = np.load(root_dir / feat_file)
         
         feats.append(raw_feats[group.start_end_indices.to_numpy(), :])
         labels.append(group.label.to_numpy())
         
-    X = np.concatenate(feats, axis=0)
-         
+    X = np.concatenate(feats, axis=0)    
     y = np.concatenate(labels)
     
+    return X, y
+
+
+def train_mlp_regressor(
+    train_data, module, log_dir,
+    criterion=torch.nn.MSELoss,
+    optim=torch.optim.SGD,
+    batch=625,
+    random_state=42,
+    input_dim=768,
+    h_dim=128,
+    
+    ):
+    
+    torch.manual_seed(random_state)
+    callbacks = []
+
+    # Clip gradients to L2-norm of 2.0
+    callbacks.append(
+        ('clipping', GradientNormClipping(2.0)))
+
+    # Allow early stopping.
+    callbacks.append(
+        ('EarlyStop', EarlyStopping()))
+
+    # Instantiate our classifier.
+    model = NeuralNetRegressor(
+        # Network parameters.
+        module, module__out_dim=1, module__hidden_dim=h_dim,
+        module__in_dim=input_dim,
+        # Training batch/time/etc.
+        max_epochs=50, batch_size=batch,
+        # Training loss.
+        criterion=criterion,
+        #criterion__weight=weights,
+        # Optimization parameters.
+        optimizer=optim,
+        # Parallelization.
+        iterator_train__shuffle=True,
+        iterator_train__num_workers=4,
+        iterator_valid__num_workers=4,
+        # Scoring callbacks.
+        callbacks=callbacks,
+        train_split=False,
+        device='cuda'
+        )
+    
+    params = {
+    'lr': [0.01, 0.003, 3e-4],
+    'max_epochs': [10, 20],
+    'module__h_dim': [128, 384, 512],
+    }
+
+    gs = GridSearchCV(model, params, refit=False, scoring=['mean_squared_error', 'r2_score'], cv=3)
+    gs.fit(train_data[0], train_data[1])
+    
+    log_cross_validation(gs.cv_results_, log_dir)
+    return gs.best_estimator_
+
+def train_mlp_classifier(train_data, module,
+    log_dir,
+    criterion=torch.nn.CrossEntropyLoss,
+    optim=torch.optim.SGD,
+    batch=625,
+    random_state=42,
+    input_dim=768,
+    h_dim=128,
+    out_dim=2,
+    ):
+    
+    torch.manual_seed(random_state)
+    callbacks = []
+
+    # Clip gradients to L2-norm of 2.0
+    callbacks.append(
+        ('clipping', GradientNormClipping(2.0)))
+
+    # Allow early stopping.
+    callbacks.append(
+        ('EarlyStop', EarlyStopping()))
+
+    # Instantiate our classifier.
+    model = NeuralNetRegressor(
+        # Network parameters.
+        module, module__out_dim=out_dim, module__hidden_dim=h_dim,
+        module__in_dim=input_dim,
+        # Training batch/time/etc.
+        max_epochs=50, batch_size=batch,
+        # Training loss.
+        criterion=criterion,
+        #criterion__weight=weights,
+        # Optimization parameters.
+        optimizer=optim,
+        # Parallelization.
+        iterator_train__shuffle=True,
+        iterator_train__num_workers=4,
+        iterator_valid__num_workers=4,
+        # Scoring callbacks.
+        callbacks=callbacks,
+        train_split=False,
+        device='cuda'
+        )
+    scores = {'f1_score': make_scorer(f1_score_func),
+        'acc': 'accuracy_score',
+        'roc': make_scorer(roc_auc_score_proba, response_method='predict_proba'),
+        'log_loss': 'log_loss'
+        }
+    params = {
+    'lr': [0.01, 0.003, 3e-4],
+    'max_epochs': [10, 20],
+    'module__h_dim': [128, 384, 512],
+    }
+
+    gs = GridSearchCV(model, params, refit=False, scoring=scores, cv=3)
+    gs.fit(train_data[0], train_data[1])
+    
+    log_cross_validation(gs.cv_results_, log_dir) 
+    return gs.best_estimator_
+
+def train_regression(train_data, log_dir):
+
+    X, y = train_data
     linear_model = LinearRegression()
     cv = cross_validate(linear_model, X, y, scoring=['mean_squared_error', 'r2_score'], cv=10, n_jobs=-1)
-    log_cross_validation(cv.scores)
+    log_cross_validation(cv.scores, log_dir)
     
     linear_model.fit(X, y)
     
     return linear_model
 
 
-def train_logistic_regression(train_data, root_dir, binary=False):
+def train_logistic_classifier(train_data, log_dir, binary=False):
     
-    feats = list()
-    labels = list()
-    for name, group in train_data.groupby('file_id'):
-        feat_file = name + '.npy'
-        raw_feats = np.load(root_dir / feat_file)
-        
-        feats.append(raw_feats[group.start_end_indices.to_numpy(), :])
-        labels.append(group.label.to_numpy())
-        
-    X = np.concatenate(feats, axis=0)
-         
-    y = np.concatenate(labels)
+    X, y = train_data
     
     # Define parameters for search and scoring strategy.
     param_grid = [{
@@ -91,29 +205,9 @@ def train_logistic_regression(train_data, root_dir, binary=False):
     
     gs.fit(X, y)
 
-    log_cross_validation(gs.cv_results_)
+    log_cross_validation(gs.cv_results_, log_dir)
     
-    return gs
-
-
-def train_skorch_mlp(train_data, regression_model):
-    
-    net_regr = NeuralNetRegressor(
-        regression_model,
-        max_epochs=20,
-        lr=0.1,
-        #     device='cuda',  # uncomment this to train with CUDA
-    )
-    params = {
-    'lr': [0.05, 0.1],
-    'module__num_units': [10, 20],
-    'module__dropout': [0, 0.5],
-    'optimizer__nesterov': [False, True],
-    }
-    gs = GridSearchCV(net_regr, params, refit=False, cv=5, scoring='mean_squared_error', verbose=2)
-    
-    gs.fit()
-    pass    
+    return gs.best_estimator_
 
 
 def main():
@@ -167,7 +261,7 @@ def main():
     """
     
     #train_test_split
-    log_path = Path(f"logs/{args.corpus_name}/{args.task}/{args.model}/{args.probe}")
+    log_path = Path(f"logs/{args.corpus_name}/{args.task}/{args.probe}")
     neural_dim = args.neural_dim
     if args.task == 'f0':
         out_dim = 1
@@ -181,89 +275,46 @@ def main():
     hidden_dim = args.hidden_dim
     root_dir = args.root_dir / args.model / f'layer-{args.layer}'
     mlp = args.probe
-    csv_path = args.labels
+    csv_data = pd.read_csv(args.labels)
     train, test = train_test_split(csv_data, test_size=0.2, random_state=42)
-    
-    if regression:
-        
-        if mlp == 'mlp':
-            
-            model = MLPRegressor(neural_dim, 1, hidden_dim)
-        
-        else:
-            
-            #train, test = train_test_split(csv_data, test_size=0.2, random_state=42, shuffle=True)
-            model = train_regression(train, root_dir) 
-            
-        litModel = LitRegressor(neural_dim, 1, hidden_dim=hidden_dim)
-    
-    else:
-        if mlp == 'mlp':
-            model = MLPClassifier(neural_dim, out_dim, hidden_dim)
-        
-        else:
-            
-            model = train_logistic_regression(train, root_dir)
-            
-                        
-        litModel = LitClassifier(model, neural_dim, out_dim, hidden_dim=hidden_dim)
-        
-    csv_data = pd.read_csv(csv_path)
-    
-    if args.probe == 'linear':
-        
-        train, test = train_test_split(csv_data, test_size=0.2, random_state=42, shuffle=True)
-        
-        model = train_regression(train, root_dir, filter_silences=True)
-        test_log_regression()
-    
-    elif args.probe == 'mlp':
-        train, test = train_test_split(csv_data, test_size=0.2, random_state=42, shuffle=True)
-        
-        train, val = train_test_split(train, test_size=0.1, random_state=550, shuffle=True)
-        
-        train.sort_values(by=['file_id', 'start'], inplace=True)
-        test.sort_values(by=['file_id', 'start'], inplace=True)
-        val.sort_values(by=['file_id', 'start'], inplace=True)
-        
-        os.makedirs('tmp_datasets', exist_ok=True)
-        train.to_csv(f'tmp_datasets/train_{args.task}.csv')
-        test.to_csv(f'tmp_datasets/test_{args.task}.csv')
-        val.to_csv(f'tmp_datasets/val_{args.task}.csv')
-        
-        train_loader = DataLoader(BaseDataset(Path(f'tmp_datasets/train_{args.task}.csv'), root_dir, tensor=True, filter_silences=True), batch_size=64)
-        test_loader = DataLoader(BaseDataset(Path(f'tmp_datasets/test_{args.task}.csv'), root_dir, tensor=True, filter_silences=True), batch_size=64)
-        val_loader = DataLoader(BaseDataset(Path(f'tmp_datasets/val_{args.task}.csv'), root_dir, tensor=True, filter_silences=True), batch_size=64)
-        
-        tb_logger = pl_loggers.TensorBoardLogger(save_dir=log_path)
-        trainer = L.Trainer(
-            devices=args.gpu_count, accelerator='cuda',
-            callbacks=[EarlyStopping(monitor="val_loss", mode="min")],#, ModelSummary(model, max_depth=-1)],
-            #fast_dev_run=True,
-            #num_sanity_val_steps=2,
-            logger=tb_logger
-            )
-    
-        trainer.fit(litModel, train_loader, val_loader)
-        trainer.test(litModel, dataloaders=test_loader)
-        trainer.save_checkpoint('logs/current_model.ckpt')
+    train_set = get_full_dataset(train, root_dir)
+    cv_log_dir = log_path / f"cross_val_results.csv"
+    print('Starting training loop...') 
+    if mlp == 'mlp':
         if regression:
-            model = LitRegressor.load_from_checkpoint("logs/current_model.ckpt", model_class=model, in_dim=neural_dim, out_dim=out_dim)
+            model = train_mlp_regressor(train_set, MLPRegressor, cv_log_dir, out_dim=out_dim)
         else:
-            model = LitClassifier.load_from_checkpoint("logs/current_model.ckpt", model_class=model, in_dim=neural_dim, out_dim=out_dim)
-        model.eval()
-        test_results = list()
-        test_dataset = BaseDataset(Path(f'tmp_datasets/test_{args.task}.csv'), root_dir, tensor=True, filter_silences=True)
-        with torch.no_grad():
-            for idx in test.index:
-                test_results.append(model(test_dataset[idx][0]))
-        test[f'{mlp}_preds'] = test_results
-        os.makedirs(f'logs/test_results/', exist_ok=True)
-        test.to_csv(f'logs/test_results/layer-{args.layer}_{args.task}.csv')
-        
-        summary = ModelSummary(model, max_depth=-1)
-        print(summary)
+            model = train_mlp_classifier(train_set, MLPClassifier, cv_log_dir, out_dim=out_dim)
+    else:
+        if regression:
+            model = train_regression(train_set, cv_log_dir)
+        else:
+            model = train_logistic_classifier(train_set, cv_log_dir)
     
+    print('Done training!')
+    print('Beginning test loop...')
+    test_feats, test_labels = get_full_dataset(test, root_dir)
+    y_pred = model.predict(test_feats)
+    
+    print()
+    print('Testing done, outputting results...')
+    results = pd.DataFrame({
+        'file_id': test.file_id, 'neural_index': test.start_end_indices, 'y_pred': y_pred, 'y_true': test_labels
+    })
+   
+    results.to_csv(f"{log_path}/layer_{args.layer}.csv") 
+    
+    print(f"Results for {args.model} on {args.task} with {args.probe} on layer {args.layer}")
+    if not regression:
+        print("layer\tcorpus\tf1_score\taccuracy\tprecision\trecall")
+        print(f"{args.layer}\t{args.corpus}\t{f1_score(test_labels, y_pred)}\t{accuracy_score(test_labels,y_pred)}\t\
+            {precision_score(test_labels, y_pred)}\t{recall_score(test_labels, y_pred)}")
+        
+    else:
+        print("layer\tcorpus\tmse\tr2")
+        print(f"{args.layer}\t{args.corpus}\t{mean_squared_error(test_labels, y_pred)}\{r2_score(test_labels, y_pred)}")
+        
+    print() 
     
     
 if __name__ == '__main__':
